7cdf5dadb74d5279842bc2ba150375ffbe85c79d
riomljr <22552727+riomljr@users.noreply.github.com>
2023-01-12 21:50:13 -0600

    add get peak hour method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,8 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
+require 'date'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
@@ -35,6 +37,10 @@ def clean_phone_numbers(phone)
   number = phone.gsub(/[^\d]/, "").rjust(10,'0')[0..9]
 end
 
+def get_time(day)
+ get_hour = DateTime.strptime(day, '%m/%d/%Y %H:%M')
+ get_hour.hour
+end
 
 puts 'EventManager initialized.'
 
@@ -46,6 +52,14 @@ contents = CSV.open(
 
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
+peak_hour = []
+
+def find_hour(hours)
+  hours.reduce(Hash.new(0)) do |result, hour|
+    result[hour] += 1
+    result
+  end
+end
 
 contents.each do |row|
   id = row[0]
@@ -54,8 +68,11 @@ contents.each do |row|
   legislators = legislators_by_zipcode(zipcode)
   phone = clean_phone_numbers(row[:homephone])
 
-  p phone
+  peak_hour.push(get_time(row[:regdate]))
   #form_letter = erb_template.result(binding)
 
   #save_thank_you_letter(id,form_letter)
 end
+p find_hour(peak_hour)
+
+
