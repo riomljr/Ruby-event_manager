42ae60eb978ca517295d89301428f017e5c1def1
riomljr <22552727+riomljr@users.noreply.github.com>
2023-01-12 22:17:16 -0600

    add popular day method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -39,8 +39,14 @@ end
 
 def get_time(day)
  get_hour = DateTime.strptime(day, '%m/%d/%Y %H:%M')
- get_hour.hour
+ get_hour.strftime("%l %P")
 end
+
+def get_day(date)
+  day_of_the_week = DateTime.strptime(date, '%m/%d/%Y %H:%M')
+  Date::DAYNAMES[day_of_the_week.wday]
+end
+
 
 puts 'EventManager initialized.'
 
@@ -52,12 +58,13 @@ contents = CSV.open(
 
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
-peak_hour = []
+hours = []
+days =[]
 
-def find_hour(hours)
+def find_most_of(hours)
   hours.reduce(Hash.new(0)) do |result, hour|
     result[hour] += 1
-    result
+    result 
   end
 end
 
@@ -68,11 +75,15 @@ contents.each do |row|
   legislators = legislators_by_zipcode(zipcode)
   phone = clean_phone_numbers(row[:homephone])
 
-  peak_hour.push(get_time(row[:regdate]))
+  hours.push(get_time(row[:regdate]))
+  days.push(get_day(row[:regdate]))
+  
   #form_letter = erb_template.result(binding)
 
   #save_thank_you_letter(id,form_letter)
 end
-p find_hour(peak_hour)
+p find_most_of(hours)
+p find_most_of(days)
+
 
 
